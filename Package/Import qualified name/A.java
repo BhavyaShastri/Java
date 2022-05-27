5333da45ce04c204d3067931a7e6ce89b308360a
BhavyaShastri <91321689+BhavyaShastri@users.noreply.github.com>
2022-05-27 19:26:10 +0530

    Create A.java

---
+++ Package/Import qualified name/A.java
@@ -0,0 +1,12 @@
+/*If you use fully qualified name then only declared class of this package will be accessible.
+Now there is no need to import. 
+But you need to use fully qualified name every time when you are accessing the class or interface.*/
+
+
+package pack;
+public class A
+{
+	public void mssg(){
+		System.out.println("Hello!!");
+	}
+}

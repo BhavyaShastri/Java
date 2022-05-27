f05794a27ef45ba5662520a7ce2902ffa7a43aa2
BhavyaShastri <91321689+BhavyaShastri@users.noreply.github.com>
2022-05-27 19:10:46 +0530

    Create A.java

---
+++ Package/Import package.*/A.java
@@ -0,0 +1,11 @@
+/*import package.*;------->all the classes and interfaces of this package will be accessible
+                           to the current package but not subpackages.*/
+                             
+                             
+package pack;
+public class A
+{
+	public void mssg(){
+		System.out.println("Hello!!");
+	}
+}
